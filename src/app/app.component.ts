1f298f243c5095494605d0542a3000fba91cd29b
DamianosDW <damianosdw@gmail.com>
2019-05-14 00:23:49 +0200

    Import new components in app.module.ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,57 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'infernots3-league-frontend';
+export class AppComponent
+{
+  title = 'Inferno TS3 League';
+  leagueOfLegendsLogo = '../assets/images/lol-logo.png';
+  csgoLogo = '../assets/images/csgo-logo.png';
+  showInfoContainer = true;
+  showLeagueOfLegends = false;
+  showCSGO = false;
+  username = '';
+  team = '-';
+
+  showInfoContainerPage()
+  {
+    this.showInfoContainer = true;
+    this.showLeagueOfLegends = false;
+    this.showCSGO = false;
+  }
+
+  //TODO MOVE THIS TO NEW COMPONENT
+  showLoginForm()
+  {
+    this.username = 'DamianosDW';
+  }
+
+  showRegisterForm()
+  {
+
+  }
+
+  logOut()
+  {
+    this.username = '';
+    this.team = '-';
+    this.showInfoContainer = true;
+    this.showLeagueOfLegends = false;
+    this.showCSGO = false;
+  }
+  //TODO MOVE THIS TO NEW COMPONENT
+
+
+  showLeagueOfLegendsPage()
+  {
+    this.showLeagueOfLegends = true;
+    this.showCSGO = false;
+    this.showInfoContainer = false;
+  }
+
+  showCSGOPage()
+  {
+    this.showCSGO = true;
+    this.showLeagueOfLegends = false;
+    this.showInfoContainer = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LolComponent } from './lol/lol.component';
+import { CsgoComponent } from './csgo/csgo.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule, Routes } from "@angular/router";
+import { MatchScheduleComponent } from './match-schedule/match-schedule.component';
+import { RegisteredPlayersComponent } from './registered-players/registered-players.component';
+
+const appRoutes: Routes = [
+  { path: 'leagueoflegends', component: LolComponent },
+  { path: 'csgo', component: CsgoComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LolComponent,
+    CsgoComponent,
+    FooterComponent,
+    MatchScheduleComponent,
+    RegisteredPlayersComponent,
   ],
   imports: [
     BrowserModule
