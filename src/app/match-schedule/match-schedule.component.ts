95476f5cd0401c6eb94c306b91fef16d6974a414
DamianosDW <damianosdw@gmail.com>
2019-05-17 16:20:50 +0200

    Update 'match-schedule' component - add styles and default info

--- src/app/match-schedule/match-schedule.component.ts
+++ src/app/match-schedule/match-schedule.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-match-schedule',
   templateUrl: './match-schedule.component.html',
   styleUrls: ['./match-schedule.component.css']
 })
-export class MatchScheduleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class MatchScheduleComponent
+{
+  matches: Array<Match>;
+  constructor()
+  {
+    this.matches = [new Match(new Date(), 'Gracz #1', 'Gracz #3'), new Match(new Date(), 'Gracz #5', 'Gracz #2')];
   }
 
+
+}
+
+class Match
+{
+  public date: Date;
+  public firstPlayer: string;
+  public secondPlayer: string;
+
+  constructor(date: Date, firstPlayer: string, secondPlayer: string)
+  {
+    this.date = date;
+    this.firstPlayer = firstPlayer;
+    this.secondPlayer = secondPlayer;
+  }
 }
