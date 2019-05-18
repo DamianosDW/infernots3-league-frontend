f53961d5c828ff0faf768436650d9700e1603656
DamianosDW <damianosdw@gmail.com>
2019-05-18 17:35:31 +0200

    Update 'registration-form' component - create methods

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AppComponent} from "../app.component";
 
 @Component({
   selector: 'app-registration-form',
@@ -7,14 +8,19 @@ import { Component } from '@angular/core';
 })
 export class RegistrationFormComponent
 {
-  private login: string;
-  private password: string;
-  private ts3Nickname: string;
+  login = '';
+  password = '';
+  ts3Nickname = '';
 
-  constructor() { }
+  constructor(private appComponent: AppComponent) { }
 
-  createUserAccount()
+  createUserAccount() //TODO USE API
   {
-    alert('Konto zostało utworzone!\nUzupełnij nick (zakładka "Profil"), którym posługujesz się w wybranej grze i dołącz do rozgrywek ;)\nPowodzenia');
+    alert('Konto zostało utworzone!\nUzupełnij nick (zakładka "Profil"), którym posługujesz się w wybranej grze i dołącz do rozgrywek ;)\nPowodzenia!');
+  }
+
+  isButtonDisabled()
+  {
+    return !(this.login.length > 0 && this.password.length >= 6 && this.ts3Nickname.length > 0);
   }
 }
