05b89543d6eb676643fdc21e06f1e09d385d9e22
DamianosDW <damianosdw@gmail.com>
2019-05-22 00:31:44 +0200

    Update 'profile' component - use API to update user profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { AppComponent } from "../app.component";
+import {AppComponent, UserInfo} from "../app.component";
+import {HttpService} from "../http.service";
 
 @Component({
   selector: 'app-profile',
@@ -8,17 +9,37 @@ import { AppComponent } from "../app.component";
 })
 export class ProfileComponent
 {
-  ts3Nickname = '';
-  lolNickname = '';
-  csgoNickname = '';
+  ts3Nickname = this.appComponent.userInfo.ts3Nickname;
+  lolNickname = this.appComponent.userInfo.lolNickname;
+  csgoNickname = this.appComponent.userInfo.csgoNickname;
+  updatedUserInfo: UserInfo;
 
-  constructor(private appComponent: AppComponent) { }
+  constructor(private appComponent: AppComponent, private httpService: HttpService) { }
 
   updateProfileInfo()
   {
     if(this.ts3Nickname.length > 0 || (this.lolNickname.length > 0 || this.csgoNickname.length > 0))
     {
-      //TODO SAVE PROFILE INFO TO DB BY USING API
+      // Prepare necessary data
+      this.updatedUserInfo = {
+        userId: this.appComponent.userInfo.userId,
+        username: this.appComponent.userInfo.username,
+        ts3Nickname:  this.ts3Nickname,
+        lolNickname: this.lolNickname,
+        csgoNickname: this.csgoNickname
+      };
+
+      this.httpService.updateUserInfo(this.updatedUserInfo).subscribe(userInfoUpdated => {
+        if(userInfoUpdated)
+        {
+          this.appComponent.userInfo.ts3Nickname = this.ts3Nickname;
+          this.appComponent.userInfo.lolNickname = this.lolNickname;
+          this.appComponent.userInfo.csgoNickname = this.csgoNickname;
+          alert("Zaktualizowano profil!");
+        }
+        else
+          alert("Nie udało się zaktualizować profilu! Spróbuj ponownie później.");
+      });
     }
   }
 }
