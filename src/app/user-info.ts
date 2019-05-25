28ff20ae2a03e5562534d61e594d9f56fff3519c
DamianosDW <damianosdw@gmail.com>
2019-05-25 15:21:40 +0200

    Update UserInfo

--- src/app/user-info.ts
+++ src/app/user-info.ts
@@ -1,42 +1,60 @@
-export class UserInfo
+export interface UserInfo
 {
-  private userId?: number;
-  private username?: string;
-  private ts3Nickname?: string;
-  private lolNickname?: string;
-  private csgoNickname?: string;
-
-  constructor(userId: number, username: string, ts3Nickname: string, lolNickname: string, csgoNickname: string)
-  {
-    this.userId = userId;
-    this.username = username;
-    this.ts3Nickname = ts3Nickname;
-    this.lolNickname = lolNickname;
-    this.csgoNickname = csgoNickname;
-  }
-
-  public getUserId(): number
-  {
-    return this.userId;
-  }
-
-  public getUsername(): string
-  {
-    return this.username;
-  }
-
-  public getTs3Nickname(): string
-  {
-    return this.ts3Nickname;
-  }
-
-  public getLolNickname(): string
-  {
-    return this.lolNickname;
-  }
-
-  public getCsgoNickname(): string
-  {
-    return this.csgoNickname;
-  }
+  userId?: number;
+  username?: string;
+  ts3Nickname?: string;
+  lolNickname?: string;
+  csgoNickname?: string;
 }
+// export class UserInfo
+// {
+  // private userId?: number;
+  // private username?: string;
+  // private ts3Nickname?: string;
+  // private lolNickname?: string;
+  // private csgoNickname?: string;
+  //
+  // constructor(userId: number, username: string, ts3Nickname: string, lolNickname: string, csgoNickname: string)
+  // {
+  //   this.userId = userId;
+  //   this.username = username;
+  //   this.ts3Nickname = ts3Nickname;
+  //   this.lolNickname = lolNickname;
+  //   this.csgoNickname = csgoNickname;
+  // }
+  //
+  // public getUserId(): number
+  // {
+  //   return this.userId;
+  // }
+  //
+  // public getUsername(): string
+  // {
+  //   return this.username;
+  // }
+  //
+  // public getTs3Nickname(): string
+  // {
+  //   return this.ts3Nickname;
+  // }
+  //
+  // public getLolNickname(): string
+  // {
+  //   return this.lolNickname;
+  // }
+  //
+  // public getCsgoNickname(): string
+  // {
+  //   return this.csgoNickname;
+  // }
+  //
+  // public setUsername(username: string): void
+  // {
+  //   this.username = username;
+  // }
+  //
+  // public setTs3Nickname(ts3Nickname: string): void
+  // {
+  //   this.ts3Nickname = ts3Nickname;
+  // }
+// }
