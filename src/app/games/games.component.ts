4216a4961953b0c5bc3f3ac34618b6b1901c5bbe
DamianosDW <damianosdw@gmail.com>
2019-05-27 16:26:30 +0200

    Update 'games' component - show league points

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {AppComponent} from "../app.component";
 import {Router} from "@angular/router";
+import {UserStats} from "../userstats";
+import {HttpService} from "../http.service";
+import {UserService} from "../user.service";
 
 @Component({
   selector: 'app-games',
@@ -13,19 +16,31 @@ export class GamesComponent implements OnInit
   leagueOfLegendsLogo = '../../assets/images/lol-logo.png';
   csgoLogo = '../../assets/images/csgo-logo.png';
 
-  constructor(private appComponent: AppComponent, private router: Router) { }
+  constructor(private appComponent: AppComponent, private router: Router, private httpService: HttpService, private userService: UserService) { }
 
   ngOnInit() {
   }
 
   showLeagueOfLegendsPage()
   {
-    this.router.navigateByUrl('leagueoflegends');
+    // Set user league points and redirect to lol page
+    this.httpService.getUserStats(this.userService.getUserInfo().ts3Nickname).subscribe(userStatsData => {
+      let userStats: UserStats = userStatsData;
+
+      this.userService.setUserLeaguePoints(userStats.leaguePoints);
+      this.router.navigateByUrl('leagueoflegends');
+    });
   }
 
   showCSGOPage()
   {
-    this.router.navigateByUrl('csgo');
+    // Set user league points and redirect to csgo page
+    this.httpService.getUserStats(this.userService.getUserInfo().ts3Nickname).subscribe(userStatsData => {
+      let userStats: UserStats = userStatsData;
+
+      this.userService.setUserLeaguePoints(userStats.leaguePoints);
+      this.router.navigateByUrl('csgo');
+    });
   }
 
 }
