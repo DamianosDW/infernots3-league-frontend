f15f20a93f9493e6657297825fb66bdf4b50f0cf
DamianosDW <damianosdw@gmail.com>
2019-05-31 20:24:32 +0200

    Create another methods in HttpService and update getUserInfo()

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import {UserInfo} from "./user-info";
+import {UserStats} from "./userstats";
 
 @Injectable({
   providedIn: 'root'
@@ -19,7 +20,7 @@ export class HttpService {
 
   getUserInfo(login: string)
   {
-    return this.httpClient.get('http://localhost:8080/api/users/' + login + '/info');
+    return this.httpClient.get<UserInfo>('http://localhost:8080/api/users/' + login + '/info');
   }
 
   createAccount(ts3Nickname: string, login: string, password: string)
@@ -36,9 +37,24 @@ export class HttpService {
     return this.httpClient.put('http://localhost:8080/api/users/info/update', userInfo);
   }
 
+  getRanking()
+  {
+    return this.httpClient.get<Array<UserStats>>('http://localhost:8080/api/userstats/ranking');
+  }
+  
   getUserStats(ts3Nickname: string)
   {
-    return this.httpClient.get('http://localhost:8080/api/userstats/' + ts3Nickname);
+    return this.httpClient.get<UserStats>('http://localhost:8080/api/userstats/' + ts3Nickname);
+  }
+
+  setUserParticipationInTournament(userId: number)
+  {
+    return this.httpClient.put<boolean>('http://localhost:8080/api/userstats/update/participation/' + userId, null);
+  }
+
+  getUserId(username: string)
+  {
+    return this.httpClient.get<number>('http://localhost:8080/api/users/' + username + '/userId');
   }
 
 
